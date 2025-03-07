d3bd0a31f84749b563cb16b35b6958dfb19158be
peterho77 <hctd123@gmail.com>
2025-03-07 20:58:44 +0700

    Add Identity to BookWeb

--- Book.DataAccess/Data/ApplicationDbContext.cs
+++ Book.DataAccess/Data/ApplicationDbContext.cs
@@ -5,16 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Book.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace Book.DataAccess.Data
 {
-	public class ApplicationDbContext : DbContext
-	{
+	public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 		public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
+            base.OnModelCreating(modelBuilder);
+
 			modelBuilder.Entity<Category>().HasData(
 				new Category() { Id = 1, Name = "Scifi", DisplayOrder = 1 },
 				new Category() { Id = 2, Name = "Action", DisplayOrder = 2 },
