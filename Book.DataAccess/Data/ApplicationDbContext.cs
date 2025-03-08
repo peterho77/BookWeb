18c4662e072c524dd1b2d6f039daee79a8c5cdab
peterho77 <hctd123@gmail.com>
2025-03-08 21:15:39 +0700

    Change IdentityUser to ApplicationUser

--- Book.DataAccess/Data/ApplicationDbContext.cs
+++ Book.DataAccess/Data/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace Book.DataAccess.Data
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 		public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
             base.OnModelCreating(modelBuilder);
