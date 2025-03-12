faa2e20c49b4f97f1689f3aa6eca352a809612e9
peterho77 <hctd123@gmail.com>
2025-03-12 19:39:32 +0700

    Reapply "Add appsettingjson to .gitingore"
    This reverts commit a1d0967351a46a31631b58a418666c24f2ba5456.

---
+++ Book.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,25 @@
+﻿using Book.DataAccess.Data;
+using Book.DataAccess.Repository.IRepository;
+using Book.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db { get; set; }
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company company)
+        {
+            
+        }
+    }
+}
