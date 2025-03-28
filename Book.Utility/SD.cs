3e7416172231e1d93fec4646d325ba3fefd9934b
peterho77 <hctd123@gmail.com>
2025-03-28 08:53:38 +0700

    Revert "remove SD class from tracking"
    This reverts commit 7c7cedc54d7d8f0fec179f7d0aea64b6aaf124ba.

---
+++ Book.Utility/SD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Utility
+{
+    public static class SD
+    {
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+
+        public const string StatusPending = "Pending";
+		public const string StatusApproved = "Approved";
+		public const string StatusInProcess = "Processing";
+		public const string StatusShipped = "Shipped";
+		public const string StatusCancelled = "Cancelled";
+		public const string StatusRefunded = "Refunded";
+
+		public const string PaymentStatusPending = "Pending";
+		public const string PaymentStatusApproved = "Approved";
+		public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+		public const string PaymentStatusRejected = "Rejected";
+
+		public const string PaymentMethod_Vnpay = "VnPay";
+        public const string PaymentMethod_Stripe = "Stripe";
+
+		public const string SessionCart = "SessionShoppingCart";
+
+		
+    }
+}
